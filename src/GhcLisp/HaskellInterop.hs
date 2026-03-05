18a2ccfeb31fb334c96821a9dc16c66cd99ea76d
Ji-Haeng Huh <jhhuh.note@gmail.com>
2026-03-06 02:42:45 +0900

    feat: haskell-call interop — Int->Int via RTS C bridge, list ops via marshaling

---
+++ src/GhcLisp/HaskellInterop.hs
@@ -0,0 +1,61 @@
+{-# LANGUAGE OverloadedStrings #-}
+module GhcLisp.HaskellInterop
+  ( defaultEnvWithInterop
+  ) where
+
+import Data.IORef
+import qualified Data.Map.Strict as Map
+import Data.Text (Text)
+import qualified Data.Text as T
+import Foreign.StablePtr
+
+import GhcLisp.Types
+import GhcLisp.Eval (defaultEnv)
+import GhcLisp.RtsBridge (bridgeApplyIntInt)
+
+-- | Default environment extended with haskell-call
+defaultEnvWithInterop :: IO Env
+defaultEnvWithInterop = do
+  env <- defaultEnv
+  modifyIORef' env $ Map.insert "haskell-call" $
+    LPrimitive "haskell-call" haskellCall
+  pure env
+
+haskellCall :: [LispVal] -> IO LispVal
+haskellCall [LStr name, arg] = dispatchHaskellCall name arg
+haskellCall _ = error "haskell-call expects (haskell-call \"name\" arg)"
+
+dispatchHaskellCall :: Text -> LispVal -> IO LispVal
+-- Int -> Int functions: route through C bridge (rts_apply/rts_eval)
+dispatchHaskellCall "succ" (LInt n) = do
+  sp <- newStablePtr (succ :: Int -> Int)
+  result <- bridgeApplyIntInt sp (fromIntegral n)
+  freeStablePtr sp
+  pure $ LInt (fromIntegral result)
+
+dispatchHaskellCall "negate" (LInt n) = do
+  sp <- newStablePtr (negate :: Int -> Int)
+  result <- bridgeApplyIntInt sp (fromIntegral n)
+  freeStablePtr sp
+  pure $ LInt (fromIntegral result)
+
+-- List functions: Haskell-side marshaling
+dispatchHaskellCall "reverse" listVal = do
+  let hs = toHaskellListInt listVal
+  pure $ fromHaskellListInt (reverse hs)
+
+dispatchHaskellCall "length" listVal = do
+  let hs = toHaskellListInt listVal
+  pure $ LInt (fromIntegral (length hs))
+
+dispatchHaskellCall name _ = error $ "unknown Haskell function: " <> T.unpack name
+
+-- | Marshal LispVal cons list to Haskell [Int]
+toHaskellListInt :: LispVal -> [Int]
+toHaskellListInt LNil = []
+toHaskellListInt (LCons (LInt n) rest) = fromIntegral n : toHaskellListInt rest
+toHaskellListInt _ = error "expected list of integers"
+
+-- | Marshal Haskell [Int] to LispVal cons list
+fromHaskellListInt :: [Int] -> LispVal
+fromHaskellListInt = foldr (\x acc -> LCons (LInt (fromIntegral x)) acc) LNil
---
+++ test/InteropSpec.hs
@@ -0,0 +1,33 @@
+module InteropSpec (spec) where
+
+import Test.Hspec
+import qualified Data.Text as T
+import GhcLisp.Types
+import GhcLisp.Eval
+import GhcLisp.Parser
+import GhcLisp.Printer
+import GhcLisp.HaskellInterop
+
+-- Helper: parse + eval with interop, return printed result
+run :: String -> IO String
+run input = do
+  env <- defaultEnvWithInterop
+  case parseLisp (T.pack input) of
+    Left err -> error (show err)
+    Right expr -> do
+      val <- eval env expr
+      pure (printVal val)
+
+spec :: Spec
+spec = describe "Haskell Interop" $ do
+  it "calls succ on an Int" $
+    run "(haskell-call \"succ\" 41)" `shouldReturn` "42"
+
+  it "calls negate on an Int" $
+    run "(haskell-call \"negate\" 10)" `shouldReturn` "-10"
+
+  it "calls reverse on a list of Ints" $
+    run "(haskell-call \"reverse\" (list 1 2 3))" `shouldReturn` "(3 2 1)"
+
+  it "calls length on a list" $
+    run "(haskell-call \"length\" (list 10 20 30))" `shouldReturn` "3"
