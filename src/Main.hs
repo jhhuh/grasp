62e1524ddc32bdc6223f7ce601c7e9285506fa2d
Ji-Haeng Huh <jhhuh.note@gmail.com>
2026-03-09 23:49:46 +0900

    feat: add minimal REPL with isocline

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,35 @@
-module Main where
+{-# LANGUAGE OverloadedStrings #-}
+module Main (main) where
+
+import qualified Data.Text as T
+import System.Console.Isocline
+import Control.Exception (SomeException, catch)
+
+import Grasp.Parser (parseLisp)
+import Grasp.Eval (eval, defaultEnv)
+import Grasp.Printer (printVal)
+import Grasp.Types (Env)
 
 main :: IO ()
-main = putStrLn "grasp v2"
+main = do
+  setHistory "grasp_history" 200
+  env <- defaultEnv
+  putStrLn "grasp v2"
+  repl env
+
+repl :: Env -> IO ()
+repl env = do
+  mline <- readlineExMaybe "grasp> " Nothing Nothing
+  case mline of
+    Nothing -> putStrLn "bye."
+    Just line
+      | all (== ' ') line || null line -> repl env
+      | otherwise -> do
+          case parseLisp (T.pack line) of
+            Left err -> putStrLn $ "parse error: " <> show err
+            Right expr -> (do
+              result <- eval env expr
+              putStrLn (printVal result))
+              `catch` \(e :: SomeException) ->
+                putStrLn $ "error: " <> show e
+          repl env
